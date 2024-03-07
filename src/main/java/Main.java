edfe9600fcf870513ccd05d6b0c9fa069e8e2ff7
Naamafle <127473586+Naamafle@users.noreply.github.com>
2024-03-07 23:38:32 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -181,6 +181,7 @@ class Main {
         return false;
     }
 
+  //ex17
     public static int p151_ex2(Queue<Time> q) {
         Queue<Time> temp = cloneQueueTime(q);
         Time firstTime = temp.remove();
@@ -191,6 +192,101 @@ class Main {
         int firstInSeconds = firstTime.getHours() * 3600 + firstTime.getMinutes() * 60 + firstTime.getSeconds();
         int lastInSeconds = lastTime.getHours() * 3600 + lastTime.getMinutes() * 60 + lastTime.getSeconds();
         return lastInSeconds - firstInSeconds;
+    }
+
+  //ex18
+      public static Queue<Ball> fill(String[] a, int n){
+        Queue<Ball> q=new Queue<Ball>();
+        int r1;
+        int r2;
+        for(int i=0;i<n;i++) {
+            r1 = (int )(Math.random() * a.length);
+            r2 = (int )(Math.random() * 2);
+            Ball b=new Ball(r1==1,a[r1]);
+            q.insert(b);
+        }
+        return q;
+    }
+    public static int place(Queue<Ball> q, Ball b) {
+        int i=0;
+        q.insert(null);
+        while(q!=null){
+            if(q.head().getColor().equals(b)){
+                return i;
+            }
+            i++;
+            q.insert(q.remove());
+        }
+        return i;
+    }
+
+    public static void p152_ex4(){
+        int counter=0;
+        String[] arr= {"Yellow","Blue","Green","Purple","Red","Gold"};
+        Queue<Ball> q=fill(arr,20);
+        int place = place(q,new Ball(true,"Gold"));
+
+        System.out.println("enter a guess: ");
+        int guess = reader.nextInt();
+
+        while(guess!=place){
+            System.out.println("enter a guess: ");
+            guess = reader.nextInt();
+            counter++;
+        }
+        System.out.println(counter+" many guesses");
+
+    }
+
+  //ex19
+      public static void p152_ex6(Queue<Job> q, int time) {
+        q.insert(null);
+        while(q.head()!=null) {
+            if(q.head().getTime()<time) {
+                q.remove();
+                time=time-q.head().getTime();
+            }
+            else {
+                q.insert(q.remove());
+            }
+        }
+        q.remove();
+    }
+
+  //ex20
+      public static Queue<Integer> p153_ex8(Queue<Integer> q1,Queue<Integer> q2){
+        Queue<Integer> newq=new Queue<Integer>();
+        while(!q1.isEmpty() && q2.isEmpty()){
+            if(q1.head()>=q2.head()) {
+                newq.insert(q1.remove());
+            }
+            else{
+                newq.insert(q2.remove());
+            }
+        }
+        while(!q1.isEmpty()){
+            newq.insert(q1.remove());
+        }
+        while(!q2.isEmpty()){
+            newq.insert(q2.remove());
+        }
+        return newq;
+    }
+
+  //ex21
+   public static String p153_ex9(Queue<Deliveryguy> q,int cc){
+        String saveId;
+        q.insert(null);
+        while(cc>q.head().getCc()){
+            q.insert(q.remove());
+        }
+        saveId=q.head().getId();
+        q.remove();
+        while(q.head()!=null){
+            q.insert(q.remove());
+        }
+        q.remove();
+        return saveId;
     }
  
 
